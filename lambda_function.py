ace3ed0771a68a1ed54fe25f6039045f4598b795
MosheKaplan <mokplan@gmail.com>
2022-10-31 15:49:40 +0200

    Add retries to avoid failures

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,26 @@ import json
 import requests
 import os
 import datetime
+import time
 
 MAX_DELTA_DAYS = 60
+
+def get_data(url, data, headers):
+  for i in range(1, 5):
+    r = get_data_base(url, data, headers)
+    if r != None:
+      return r
+    time.sleep(15)
+   
+  return None
+
+def get_data_base(url, data, headers):
+  try:
+    r = requests.post(url, json=data, headers=headers, verify=False)
+    return r
+  except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print('request error: {}'.format(e))
+    return None
 
 def lambda_handler(event, context):
   url = '{}/api/external/fullcalendar_events_ajax'.format(os.environ['HAPROXY_PATH'])
@@ -41,8 +59,16 @@ def lambda_handler(event, context):
     "end": to_date.strftime("%Y-%m-%d")
   }
   
-  r = requests.post(url, json=data, headers=headers, verify=False)
-  
+  r = get_data(url, data, headers)
+  if r == None:
+    return {
+      "statusCode": 200,
+      "headers": {
+         "Content-Type": "application/json"
+      },
+      "body": {"error": "fail to fetch data"}
+    }        
+      
   return {
     "statusCode": 200,
     "headers": {
