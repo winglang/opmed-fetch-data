967fcc31194ae075b2e62a7b316a03a39f4524a6
Moshe Kaplan <mokplan@gmail.com>
2022-11-15 17:02:34 +0200

    support both postman and cloudwatch calls

--- lambda_function.py
+++ lambda_function.py
@@ -41,7 +41,7 @@ def lambda_handler(event, context):
   save_to_blob = False
 
   print("event: {}".format(event))  
-  if "body" in event and "save" in event["body"]:
+  if "body" in event and "save" in event["body"] or "save" in event:
     save_to_blob = True
 
   if "queryStringParameters" in event and event["queryStringParameters"] != None:
