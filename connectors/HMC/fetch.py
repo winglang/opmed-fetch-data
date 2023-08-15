7155fad4990eaf7aea8d1c5ac3bdeb86f68040c6
nitzanpal <nitzan@greatmix.ai>
2023-08-15 12:34:29 +0300

    fixed typo

--- connectors/HMC/fetch.py
+++ connectors/HMC/fetch.py
@@ -40,7 +40,7 @@ def fetch_all_data_concurrently(url, data, headers, chunk_size=1):
                 "start": start.strftime("%Y-%m-%d"),
                 "end": end.strftime("%Y-%m-%d")
             }
-            data_to_send += [(url, data_to_send, headers)]
+            requests_data_to_send += [(url, data_to_send, headers)]
         future_to_key = [executor.submit(fetch_request, *request_data) for request_data in requests_data_to_send]
 
         for future in futures.as_completed(future_to_key):
