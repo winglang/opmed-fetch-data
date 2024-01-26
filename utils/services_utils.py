f1c91cc96d927cee5105e10a25dfae26604b4694
nitzanpal <nitzan@greatmix.ai>
2024-01-26 15:56:52 +0200

    added nbi-users

--- utils/services_utils.py
+++ utils/services_utils.py
@@ -11,8 +11,10 @@ DOMAIN_TO_USER_GROUPS = {
     'demo.greatmix.ai': {"demo-users"},
     'fhir.greatmix.ai': {"fhir-users"},
     'mayo.opmed.ai': {"mayo-users"},
+    'nbi.opmed.ai': {"nbi-users"},
     'umh-dev.greatmix.ai': {"umh-users"},
     'umh.greatmix.ai': {"umh-users"}
+
 }
 
 
@@ -20,14 +22,14 @@ class Service(Enum):
     HMC = "hmc-users"
     FHIR = "fhir-users"
     MAYO = "mayo-users"
+    NBI = "nbi-users"
     MOCK = "mock-users"
     DEMO = "demo-users"
     SANDBOX = "opmed-sandbox"
 
 
 def valid_service(service):
-    return service in [Service.HMC.value, Service.FHIR.value, Service.MOCK.value, Service.MAYO.value,
-                       Service.DEMO.value] or service.startswith(Service.SANDBOX.value)
+    return service in service in [x.value for x in Service] or service.startswith(Service.SANDBOX.value)
 
 
 def get_service_ids_from_cognito_jwt(jwt: dict) -> [str]:
