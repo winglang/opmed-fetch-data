274717250eca26f0a12f094b92a2eb30279fb73f
Shaked Matar <shaked@opmed.ai>
2024-03-06 20:46:33 +0200

    added block usage prediction folder - initial commit

---
+++ predict_block_usage/predict_block_usage/block_usage_predictor.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
+from predict_block_usage.predict_block_usage.data_analysis import compute_records_and_averages
+from predict_block_usage.utils.s3_utils import get_s3_object, put_item_in_s3
+
+SCHEDULED_SURGERIES_DB_FILE_NAME = None
+BUCKET_NAME_DB = None
+BUCKET_NAME_PREDICTIONS_OUTPUT = None
+PREDICTIONS_OUTPUT_FILE_NAME = None
+DOCTORS_TO_HASH_FILE_NAME = None
+
+def load_env():
+    global BUCKET_NAME_DB, SCHEDULED_SURGERIES_DB_FILE_NAME, BUCKET_NAME_PREDICTIONS_OUTPUT, PREDICTIONS_OUTPUT_FILE_NAME, DOCTORS_TO_HASH_FILE_NAME
+    load_dotenv()
+    BUCKET_NAME_DB = os.getenv('BUCKET_NAME_DB')
+    SCHEDULED_SURGERIES_DB_FILE_NAME = os.getenv('SCHEDULED_SURGERIES_DB_FILE_NAME')
+    BUCKET_NAME_PREDICTIONS_OUTPUT = os.getenv('BUCKET_NAME_PREDICTIONS_OUTPUT')
+    PREDICTIONS_OUTPUT_FILE_NAME = os.getenv('PREDICTIONS_OUTPUT_FILE_NAME')
+    DOCTORS_TO_HASH_FILE_NAME = os.getenv('DOCTORS_TO_HASH_FILE_NAME')
+
+
+def predict_block_usage():
+    load_env()
+    # # # Compute bins # # #
+    df_schedule = get_s3_object(SCHEDULED_SURGERIES_DB_FILE_NAME, BUCKET_NAME_DB)
+    all_surgeons_data = compute_records_and_averages(df_schedule, DOCTORS_TO_HASH_FILE_NAME, BUCKET_NAME_DB)
+    # file path is block name, file name
+    json_data = json.dumps(all_surgeons_data, indent=4, default=str)
+    put_item_in_s3(bucket=BUCKET_NAME_PREDICTIONS_OUTPUT, key=PREDICTIONS_OUTPUT_FILE_NAME, body=json_data, acl_needed=True)
+
+
+if __name__ == "__main__":
+    predict_block_usage()
---
+++ predict_block_usage/update_block_allocation_and_usage/load_data.py
@@ -0,0 +1,103 @@
+import os
+from datetime import datetime
+
+import pandas as pd
+
+from predict_block_usage.utils.s3_utils import get_s3_object
+
+
+def compute_scheduled_and_allocation_dfs(file_names):
+    scheduled_dfs = []
+    allocated_dfs = []
+    counter = 0
+    for file_name in file_names:
+        counter += 1
+        if counter % 50 == 0:
+            print(f"file number {counter}")
+        df = pd.DataFrame(get_s3_object(file_name, os.getenv("BUCKET_NAME_FETCH")))
+
+        # get the date from file name
+        file_date_str = file_name.split('.')[1][:8]  # Adjust slicing if necessary
+        file_date = datetime.strptime(file_date_str, '%Y%m%d').date()
+        df['start_date'] = pd.to_datetime(df['start']).dt.date
+
+        # # Scheduled Usage:  # #
+        # Filter rows where 'start_date' matches the date extracted from the file name
+        rows_match_dates_df = df[df['start_date'] == file_date]
+        scheduled_dfs.append(rows_match_dates_df)
+
+        # # Original Allocations: # #
+        # Remove rows for the last date in 'start', because it may be incomplete
+        last_date_in_file = df['start_date'].max()
+        df = df[df['start_date'] != last_date_in_file]
+        last_complete_date_in_file = df['start_date'].max()
+
+        # Filter rows with dates equal to
+        rows_match_last_date = df[df['start_date'] == last_complete_date_in_file]
+        allocated_dfs.append(rows_match_last_date)
+
+        # Concatenate all filtered DataFrames
+    scheduled_df = pd.concat(scheduled_dfs, ignore_index=True)
+    allocated_df = pd.concat(allocated_dfs, ignore_index=True)
+    varify_consecutive_dates(scheduled_df)  # todo: where do I raise a flag?
+    varify_consecutive_dates(allocated_df)
+
+    # compute total scheduled
+    total_scheduled_time = compute_total_scheduled_time(scheduled_df)
+    total_allocated_time = compute_total_allocation_time(allocated_df)
+
+    return total_scheduled_time, total_allocated_time
+
+
+def compute_total_scheduled_time(df):
+    current_block_row_index = 0
+    last_row_current_block = current_block_row_index + 1
+    next_block_row_index = 0
+    while next_block_row_index < len(df):
+        while last_row_current_block < len(df) and pd.isna(df.loc[last_row_current_block, 'id']):
+            last_row_current_block += 1
+        next_block_row_index = last_row_current_block
+        last_row_current_block = last_row_current_block - 1
+
+        # now i know the first and last, I need to take the maximum and minimum and put it in the first row.
+        df.loc[current_block_row_index, 'start'] = df.loc[current_block_row_index + 1: last_row_current_block,
+                                                          'start'].min()
+        df.loc[current_block_row_index, 'end'] = df.loc[current_block_row_index + 1: last_row_current_block,
+                                                        'end'].max()
+
+        current_block_row_index = next_block_row_index
+        last_row_current_block = current_block_row_index + 1
+
+    df_cleaned = df.dropna(subset=['id'])
+    df_cleaned = df_cleaned.reset_index(drop=True)
+
+    return df_cleaned
+
+
+def compute_total_allocation_time(df):
+    df_cleaned = df.dropna(subset=['id'])
+    return df_cleaned
+
+
+def varify_consecutive_dates(df):
+    df['start'] = pd.to_datetime(df['start'])
+    # Extract just the date part for easier comparison
+    df['start_date'] = df['start'].dt.date
+    # Identify the full range of dates in your DataFrame
+    min_date = df['start_date'].min()
+    max_date = df['start_date'].max()
+    # Generate a complete list of dates in this range
+    full_date_range = pd.date_range(start=min_date, end=max_date).date
+    # Find which dates from the full range are not in your DataFrame
+    missing_dates = set(full_date_range) - set(df['start_date'].unique())
+    # Now, missing_dates contains all dates that are not represented in your DataFrame
+    missing_dates = pd.to_datetime(list(missing_dates))
+    missing_dates = missing_dates.sort_values()
+
+    missing_dates.sort_values(ascending=True)
+    # Iterate through each missing date
+    for date in missing_dates:
+        # Check if the day of the week is not Saturday
+        if date.strftime('%A') != 'Saturday':
+            # Print the date and the day of the week if it's not Saturday
+            print(f"{date.date()} ({date.strftime('%A')})")
