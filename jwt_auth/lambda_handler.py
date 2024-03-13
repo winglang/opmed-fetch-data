c08c7bc3ca2b7e92f4ee80e7c898800d18805c9e
nitzanpal <nitzan@greatmix.ai>
2024-03-13 13:17:50 +0200

    extract user and organization from jwt and pass them as headers in viewer request

--- jwt_auth/lambda_handler.py
+++ jwt_auth/lambda_handler.py
@@ -10,12 +10,18 @@ def validate_view_blocks_handler(event, context):
     jwt = query_params.get('token')
     jwt_item = get_jwt_from_db(jwt)
 
-    if not jwt_item or not validate_jwt(jwt):
+    jwt_payload = validate_jwt(jwt)
+
+    if not jwt_item or not jwt_payload:
         return generate_401_response()
 
     requested_blocks = query_params['block_ids']
 
     if set(requested_blocks).issubset(jwt_item['blocks_id']):
+        if 'headers' not in request:
+            request['headers'] = {}
+        request['headers']['doctor-id'] = [{'key': 'Doctor-Id', 'value': jwt_item['user_id']}]
+        request['headers']['tenant-id'] = [{'key': 'Tenant-Id', 'value': jwt_item['org_id']}]
         return request
     else:
         return generate_403_response()
