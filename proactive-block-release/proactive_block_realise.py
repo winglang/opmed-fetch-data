4394652e2021843322fb9e5435142bf34d2b268f
nitzanpal <nitzan@greatmix.ai>
2024-03-19 17:14:41 +0200

    fixed issue where there no event body for GET requests

--- proactive-block-release/proactive_block_realise.py
+++ proactive-block-release/proactive_block_realise.py
@@ -103,7 +103,7 @@ def proactive_block_realise(event, context):
         response_body = blocks_predictions_res.text
     save_to_s3 = event.get("body", {}).get("save_to_s3", False)
     if save_to_s3:
-        s3_key = " proactive-block.json"
+        s3_key = "proactive-block.json"
         bucket_name = os.environ['BUCKET_NAME']
         try:
             s3 = boto3.resource('s3')
