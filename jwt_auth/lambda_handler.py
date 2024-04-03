ad25e5491bfa615e6618cbb784413b1cfc95b3da
nitzanpal <nitzan@greatmix.ai>
2024-04-03 21:10:32 +0300

    jwt no longer contains block ids

--- jwt_auth/lambda_handler.py
+++ jwt_auth/lambda_handler.py
@@ -1,6 +1,6 @@
 from urllib.parse import parse_qs
 
-from utils.jwt_utils import validate_jwt, generate_401_response, generate_403_response
+from utils.jwt_utils import validate_jwt, generate_401_response
 
 
 def validate_view_blocks_handler(event, context):
@@ -18,14 +18,8 @@ def validate_view_blocks_handler(event, context):
     if not jwt_payload:
         return generate_401_response()
 
-    requested_blocks_ids = query_params.get("ids").split(",") if "ids" in query_params else []
-    jwt_payload_block_ids = jwt_payload.get("block_ids", "").split(",")
-
-    if set(requested_blocks_ids).issubset(jwt_payload_block_ids):
-        if "headers" not in request:
-            request["headers"] = {}
-        request["headers"]["User-Id"] = [{"key": "User-Id", "value": jwt_payload["user_id"]}]
-        request["headers"]["Tenant-Id"] = [{"key": "Tenant-Id", "value": jwt_payload["org_id"]}]
-        return request
-    else:
-        return generate_403_response()
+    if "headers" not in request:
+        request["headers"] = {}
+    request["headers"]["User-Id"] = [{"key": "User-Id", "value": jwt_payload["user_id"]}]
+    request["headers"]["Tenant-Id"] = [{"key": "Tenant-Id", "value": jwt_payload["org_id"]}]
+    return request
