8e042da523ec08387eaf457d6f95243da86fe9fb
nitzanpal <nitzan@greatmix.ai>
2024-05-02 11:43:07 +0300

    update NotificationMessage init declaration types

--- proactive_nudge_reminder/notification_message_details.py
+++ proactive_nudge_reminder/notification_message_details.py
@@ -1,5 +1,10 @@
+from typing import List
+
+
 class NotificationMessage:
-    def __init__(self, hospital_name, recipients, content, doctor_name, link_for_surgeon):
+    def __init__(
+        self, hospital_name: str, recipients: List[str], content: str, doctor_name: str, link_for_surgeon: str
+    ):
         self.hospital_name = hospital_name
         self.recipients = recipients
         self.nudge_content = content
